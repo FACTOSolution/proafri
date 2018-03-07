025b3de5e303a14314959589aa01d71780921db1
jeovazero <jeovapereira2000@live.com>
2018-03-07 01:20:56 -0300

    routes added to Candidates

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+var Candidate = require('../models/candidate');
+// var User = require('../models/user');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/add', function(req, res, next){
+  console.log("BODY ", req.body);
+  
+  var newCand = new Candidate(req.body);
+  
+  newCand.save((err) => {
+    if(err){
+      console.log(err);
+      return res.json({success: false, message: "Erro no cadastro algum campo requerido faltando. "});
+    }
+    res.json({
+      success: true, 
+      message: "Adicionado com sucesso."
+    });
+  });
+  // Make some thing 
+  // res.render('podepah', { title: 'SOh', message: 'Guardian > Fallen' });
+  //res.json({ title: 'SOh', message: 'Guardian > Fallen' });
 });
 
 module.exports = router;
