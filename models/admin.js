33be3d30d5f078deeddb01e491d57deb11c404a6
jeovazero <jeovapereira2000@live.com>
2018-03-07 21:05:51 -0300

    Added method to verify hashs of passwords

--- models/admin.js
+++ models/admin.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var argon = require('argon2');
 
 const AdminsSchema = new Schema({
     name: String,
@@ -13,8 +14,19 @@ const AdminsSchema = new Schema({
     }
 });
 
-AdminsSchema.methods.comparePass = function(pass){
+AdminsSchema.methods.verify = function(passwd, callback){
     //compareHashHere(pass, this.hash_password);
+    argon.verify(this.hash_password, passwd).then(match => {
+        if(match){
+            console.log("Senha valida");
+            return callback(true);
+        }else{
+            console.log("Senha invalida :(");
+            return callback(false);
+        }
+    }).catch(err => {
+        console.log(err);
+    });
 }
 
-module.exports = mongoose.model('AdminsSchema', AdminsSchema);
+module.exports = mongoose.model('Admins', AdminsSchema);
