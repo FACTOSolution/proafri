d1f6d5e66341b73291cbc2a9524af9428d969085
jeovazero <jeovapereira2000@live.com>
2018-03-07 21:02:42 -0300

    added renders to user routes

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var Candidate = require('../models/candidate');
-// var User = require('../models/user');
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-router.post('/add', function(req, res, next){
-  console.log("BODY ", req.body);
-  
+/**
+ * Add a candidate to mongo
+ */
+router.post('/', function(req, res, next){
   var newCand = new Candidate(req.body);
   
   newCand.save((err) => {
     if(err){
       console.log(err);
-      return res.json({success: false, message: "Erro no cadastro algum campo requerido faltando. "});
+      //return res.json({success: false, message: "Erro no cadastro algum campo requerido faltando. "});
+      res.render('confirmation', {
+        title: "Failure",
+        message: "Erro no cadastro algum campo requerido faltando."
+      });
+    }else{
+      res.render('confirmation', {
+        title: "Success",
+        message: "Adicionado com sucesso."
+      });
     }
-    res.json({
-      success: true, 
-      message: "Adicionado com sucesso."
-    });
   });
   // Make some thing 
   // res.render('podepah', { title: 'SOh', message: 'Guardian > Fallen' });
-  //res.json({ title: 'SOh', message: 'Guardian > Fallen' });
+  // res.json({ title: 'SOh', message: 'Guardian > Fallen' });
 });
 
 module.exports = router;
