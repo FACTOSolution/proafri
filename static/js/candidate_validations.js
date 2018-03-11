036e5e902613bfd5e0d43c56500a13a4dfdac9b2
ppalbuquerque <albpedro897@hotmail.com>
2018-03-11 00:01:59 -0300

    Started the region check validator

---
+++ static/js/candidate_validations.js
@@ -0,0 +1,6 @@
+function matchRegion(sel){
+    var regionA = sel.value.split(":");
+    // RegionA is in regionA[1]
+    var regionB = $('#programB').val().split(":");
+    //  RegionB is in regionB[1]
+}
