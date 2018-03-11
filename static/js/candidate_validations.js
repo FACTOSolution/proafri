daefcf1a55ce02efa122457bcb43ed15271725c0
ppalbuquerque <albpedro897@hotmail.com>
2018-03-11 12:26:15 -0300

    Added matchRegion to validate exclusive choice in candidate_form

--- static/js/candidate_validations.js
+++ static/js/candidate_validations.js
@@ -2,5 +2,25 @@ function matchRegion(sel){
     var regionA = sel.value.split(":");
     // RegionA is in regionA[1]
     var regionB = $('#programB').val().split(":");
+    if(sel['name'] == 'programB') { regionB = $('#programA').val().split(":"); }
     //  RegionB is in regionB[1]
+    if(regionA[1] == regionB[1])
+    {
+        $('#enviar').attr('disabled','disabled');
+        $('#programA').notify(
+            'Não pode ser selecionado dois programas da mesma região.', 
+            'warn',
+            { position: 'right'}
+        );
+    } else if(regionA[0] == regionB[0]) {
+        $('#enviar').attr('disabled','disabled');
+        $('#programA').notify(
+            'Não pode ser selecionado dois programas iguais.', 
+            'warn',
+            { position: 'right'}
+        );
+    }
+    else {
+        $('#enviar').removeAttr('disabled');
+    }
 }
